96cf8e5f2831c80a59e73cd405fbd515197789c8
Jacob <Jtdawgzone@users.noreply.github.com>
2019-12-11 23:17:13 -0800

    Withholding API Key from Github for now

---
+++ js/map.js
@@ -0,0 +1,3 @@
+// MapBox API key
+// Storing here so can .gitignore (to protect key) until submission time
+const apiKey = 'pk.eyJ1IjoianRkYXdnem9uZSIsImEiOiJjazQwZzNmM2gwMWNqM2xxa3k2M2E4a3FxIn0.REbqlvZh1AXKT2TUKIGDjA';
