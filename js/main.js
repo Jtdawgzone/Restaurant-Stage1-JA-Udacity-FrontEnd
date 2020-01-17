c7b74dbf2218410bb58b228dd9bd5f621a6b74ef
Jacob <Jtdawgzone@users.noreply.github.com>
2020-01-16 23:40:01 -0800

    Adding offline mode
    - Added checks for online for map

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-let restaurants,
-  neighborhoods,
-  cuisines
-var newMap
-var markers = []
+let restaurants, neighborhoods, cuisines;
+var newMap;
+var markers = [];
 
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
-document.addEventListener('DOMContentLoaded', (event) => {
-  initMap(); // added 
+document.addEventListener("DOMContentLoaded", event => {
+  initMap(); // added
   fetchNeighborhoods();
   fetchCuisines();
 });
@@ -18,76 +16,88 @@ document.addEventListener('DOMContentLoaded', (event) => {
  */
 fetchNeighborhoods = () => {
   DBHelper.fetchNeighborhoods((error, neighborhoods) => {
-    if (error) { // Got an error
+    if (error) {
+      // Got an error
       console.error(error);
     } else {
       self.neighborhoods = neighborhoods;
       fillNeighborhoodsHTML();
     }
   });
-}
+};
 
 /**
  * Set neighborhoods HTML.
  */
 fillNeighborhoodsHTML = (neighborhoods = self.neighborhoods) => {
-  const select = document.getElementById('neighborhoods-select');
+  const select = document.getElementById("neighborhoods-select");
   neighborhoods.forEach(neighborhood => {
-    const option = document.createElement('option');
+    const option = document.createElement("option");
     option.innerHTML = neighborhood;
     option.value = neighborhood;
     select.append(option);
   });
-}
+};
 
 /**
  * Fetch all cuisines and set their HTML.
  */
 fetchCuisines = () => {
   DBHelper.fetchCuisines((error, cuisines) => {
-    if (error) { // Got an error!
+    if (error) {
+      // Got an error!
       console.error(error);
     } else {
       self.cuisines = cuisines;
       fillCuisinesHTML();
     }
   });
-}
+};
 
 /**
  * Set cuisines HTML.
  */
 fillCuisinesHTML = (cuisines = self.cuisines) => {
-  const select = document.getElementById('cuisines-select');
+  const select = document.getElementById("cuisines-select");
 
   cuisines.forEach(cuisine => {
-    const option = document.createElement('option');
+    const option = document.createElement("option");
     option.innerHTML = cuisine;
     option.value = cuisine;
     select.append(option);
   });
-}
+};
 
 /**
  * Initialize leaflet map, called from HTML.
  */
 initMap = () => {
-  self.newMap = L.map('map', {
+  if (navigator.onLine) {
+    try {
+      self.newMap = L.map("map", {
         center: [40.722216, -73.987501],
         zoom: 12,
         scrollWheelZoom: false
       });
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: apiKey,
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox.streets'
-  }).addTo(newMap);
+      L.tileLayer(
+        "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}",
+        {
+          mapboxToken: apiKey,
+          maxZoom: 18,
+          attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          id: "mapbox.streets"
+        }
+      ).addTo(newMap);
+    } catch (error) {
+      console.log("Couldn't initialize map", error);
+    }
+  }
 
   updateRestaurants();
-}
+};
 /* window.initMap = () => {
   let loc = {
     lat: 40.722216,
@@ -105,8 +115,8 @@ initMap = () => {
  * Update page and map for current restaurants.
  */
 updateRestaurants = () => {
-  const cSelect = document.getElementById('cuisines-select');
-  const nSelect = document.getElementById('neighborhoods-select');
+  const cSelect = document.getElementById("cuisines-select");
+  const nSelect = document.getElementById("neighborhoods-select");
 
   const cIndex = cSelect.selectedIndex;
   const nIndex = nSelect.selectedIndex;
@@ -114,24 +124,29 @@ updateRestaurants = () => {
   const cuisine = cSelect[cIndex].value;
   const neighborhood = nSelect[nIndex].value;
 
-  DBHelper.fetchRestaurantByCuisineAndNeighborhood(cuisine, neighborhood, (error, restaurants) => {
-    if (error) { // Got an error!
-      console.error(error);
-    } else {
-      resetRestaurants(restaurants);
-      fillRestaurantsHTML();
+  DBHelper.fetchRestaurantByCuisineAndNeighborhood(
+    cuisine,
+    neighborhood,
+    (error, restaurants) => {
+      if (error) {
+        // Got an error!
+        console.error(error);
+      } else {
+        resetRestaurants(restaurants);
+        fillRestaurantsHTML();
+      }
     }
-  })
-}
+  );
+};
 
 /**
  * Clear current restaurants, their HTML and remove their map markers.
  */
-resetRestaurants = (restaurants) => {
+resetRestaurants = restaurants => {
   // Remove all restaurants
   self.restaurants = [];
-  const ul = document.getElementById('restaurants-list');
-  ul.innerHTML = '';
+  const ul = document.getElementById("restaurants-list");
+  ul.innerHTML = "";
 
   // Remove all map markers
   if (self.markers) {
@@ -139,32 +154,32 @@ resetRestaurants = (restaurants) => {
   }
   self.markers = [];
   self.restaurants = restaurants;
-}
+};
 
 /**
  * Create all restaurants HTML and add them to the webpage.
  */
 fillRestaurantsHTML = (restaurants = self.restaurants) => {
-  const ul = document.getElementById('restaurants-list');
+  const ul = document.getElementById("restaurants-list");
   restaurants.forEach(restaurant => {
     ul.append(createRestaurantHTML(restaurant));
   });
   addMarkersToMap();
-}
+};
 
 /**
  * Create restaurant HTML.
  */
-createRestaurantHTML = (restaurant) => {
-  const li = document.createElement('li');
+createRestaurantHTML = restaurant => {
+  const li = document.createElement("li");
 
-  const name = document.createElement('h1');
+  const name = document.createElement("h1");
   name.innerHTML = restaurant.name;
   name.tabIndex = 0;
   li.append(name);
 
-  const image = document.createElement('img');
-  image.className = 'restaurant-img';
+  const image = document.createElement("img");
+  image.className = "restaurant-img";
   image.alt = `A picture of ${restaurant.name}`;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   image.srcset = DBHelper.imageSrcSetForRestaurant(restaurant);
@@ -172,30 +187,36 @@ createRestaurantHTML = (restaurant) => {
   image.tabIndex = 0;
   li.append(image);
 
-  const neighborhood = document.createElement('h3');
-  neighborhood.innerHTML = restaurant.neighborhood; 
+  const neighborhood = document.createElement("h3");
+  neighborhood.innerHTML = restaurant.neighborhood;
   neighborhood.tabIndex = 0;
   li.append(neighborhood);
 
-  const address = document.createElement('p');
+  const address = document.createElement("p");
   address.innerHTML = restaurant.address;
   address.tabIndex = 0;
   li.append(address);
 
-  const more = document.createElement('a');
-  more.innerHTML = 'Details';
+  const more = document.createElement("a");
+  more.innerHTML = "Details";
   more.href = DBHelper.urlForRestaurant(restaurant);
-  more.setAttribute("aria-label", `View more information about ${restaurant.name}`);
+  more.setAttribute(
+    "aria-label",
+    `View more information about ${restaurant.name}`
+  );
   more.tabIndex = 0;
   li.append(more);
 
   return li;
-}
+};
 
 /**
  * Add markers for current restaurants to the map.
  */
 addMarkersToMap = (restaurants = self.restaurants) => {
+  // Exit early for undefined courtesy of https://alexandroperez.github.io/mws-walkthrough/?1.25.fixing-offline-mode
+  if (!newMap || !L) return;
+
   restaurants.forEach(restaurant => {
     // Add marker to the map
     const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.newMap);
@@ -205,8 +226,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     }
     self.markers.push(marker);
   });
-
-} 
+};
 /* addMarkersToMap = (restaurants = self.restaurants) => {
   restaurants.forEach(restaurant => {
     // Add marker to the map
@@ -217,4 +237,3 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 } */
-
--- sw.js
+++ sw.js
@@ -1,3 +1,88 @@
+// Credit to Alexandro Perez for the walkthrough at
+// https://alexandroperez.github.io/mws-walkthrough/?1.23.registering-service-worker-and-caching-static-assets
+//
+//
+// Credit to https://developers.google.com/web/fundamentals/primers/service-workers
+//
+// Both elped me when I was stuck on this section
+
 const APP_NAME = "restaurant-reviews-ja";
 const STATIC_CACHE_NAME = `${APP_NAME}-v1.0`;
 const CONTENT_IMG_CACHE = `${APP_NAME}-images`;
+
+var cachesWhiteList = [STATIC_CACHE_NAME, CONTENT_IMG_CACHE];
+
+var urlsToCache = [
+  "/", // cache index.html
+  "/restaurant.html",
+  "/css/styles.css",
+  "/js/dbhelper.js",
+  "/js/main.js",
+  "/js/restaurant_info.js",
+  "js/register-sw.js",
+  "js/map.js",
+  "data/restaurants.json"
+  // We don't need to cache images
+];
+
+// Cache all assets at SW install
+self.addEventListener('install', (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(STATIC_CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cachesWhiteList.indexOf(cacheName) === -1) {
+            console.log(`Deleted cache of ${cacheName}`);
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
+
+  // Handle fetch events
+  // From https://developers.google.com/web/fundamentals/primers/service-workers
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+          .then((response) => {
+            // Cache hit - return response
+            if (response) {
+              return response;
+            }
+    
+            return fetch(event.request).then(
+              function(response) {
+                // Check if we received a valid response
+                if(!response || response.status !== 200 || response.type !== 'basic') {
+                  return response;
+                }
+    
+                // IMPORTANT: Clone the response. A response is a stream
+                // and because we want the browser to consume the response
+                // as well as the cache consuming the response, we need
+                // to clone it so we have two streams.
+                var responseToCache = response.clone();
+    
+                caches.open(STATIC_CACHE_NAME)
+                  .then(function(cache) {
+                    cache.put(event.request, responseToCache);
+                  });
+    
+                return response;
+              }
+            );
+          })
+        );
+  });
